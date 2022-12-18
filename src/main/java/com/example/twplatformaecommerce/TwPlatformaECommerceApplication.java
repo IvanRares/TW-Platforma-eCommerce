8095dce92d7fe052624b96d72f00d98a12d27dac
IvanRares <ivanrarescosmin@gmail.com>
2022-12-18 16:23:25 +0200

    Added base project

---
+++ src/main/java/com/example/twplatformaecommerce/TwPlatformaECommerceApplication.java
@@ -0,0 +1,13 @@
+package com.example.twplatformaecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwPlatformaECommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwPlatformaECommerceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/twplatformaecommerce/TwPlatformaECommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.twplatformaecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwPlatformaECommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
